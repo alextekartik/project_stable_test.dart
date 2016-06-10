add9b291505571f3824b57cf8cd5e9bc8ecdda79
Alexandre Roux Tekartik <alex@tekartik.com>
2016-06-10 18:52:24 +0200

    test: test on firefox instead of content_shell

---
+++ tool/travis.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Fast fail the script on failures.
+set -e
+
+pub run pubtest:pubtestdependencies -p vm -j 1
+
+pub run pubtest:pubtestdependencies -p firefox -j 1
+# -j 1 needed for idb_shim/websql
