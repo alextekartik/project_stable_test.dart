92414f2461e724009bbbc819eecf8a98b5b74247
Alexandre Roux Tekartik <alex@tekartik.com>
2016-06-21 09:04:50 +0200

    trigger content-shell build

--- tool/travis.sh
+++ tool/travis.sh
@@ -5,5 +5,6 @@ set -e
 
 pub run pubtest:pubtestdependencies -p vm -j 1
 
-pub run pubtest:pubtestdependencies -p firefox -j 1
+# pub run pubtest:pubtestdependencies -p firefox -j 1
+pub run pubtest:pubtestdependencies -p content-shell -j 1
 # -j 1 needed for idb_shim/websql
