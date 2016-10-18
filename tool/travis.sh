949025d6e995fd4d3a12555e18b809daee2eba5c
Alexandre Roux Tekartik <alex@tekartik.com>
2016-10-18 10:28:12 +0200

    test: trigger vm only build

--- tool/travis.sh
+++ tool/travis.sh
@@ -4,7 +4,7 @@
 set -e
 
 pub run pubtest:pubtestdependencies -p vm -j 1
-pub run pubtest:pubtestdependencies -p chrome -j 1
+# pub run pubtest:pubtestdependencies -p chrome -j 1
 # pub run pubtest:pubtestdependencies -p firefox -j 1
 #pub run pubtest:pubtestdependencies -p content-shell -j 1
 # -j 1 needed for idb_shim/websql
