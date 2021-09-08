dab05debed63d7eb5bdfde8f56e766ab98d74fde
alex <alex@tekartik.com>
2021-09-08 21:22:21 +0200

    feat: nnbd support

--- tool/travis.dart
+++ tool/travis.dart
@@ -1,14 +1,11 @@
+import 'package:dev_test/package.dart';
 import 'package:process_run/shell.dart';
 
 Future main() async {
+  await packageRunCi('.');
   var shell = Shell();
 
   await shell.run('''
-# Analyze code
-# Analyze code
-dartanalyzer --fatal-warnings --fatal-infos .
-dartfmt -n --set-exit-if-changed .
-
 # Run tests
 pub run tekartik_pubtest:pubtestdependencies.dart -p vm -j 1
 ''');
